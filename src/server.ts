0ed06c05c133ad3858f2d2c9dc3c8a8961f86eff
ayathree <nobanitaayathree333@gmail.com>
2025-12-01 00:22:20 +0600

    first

--- src/server.ts
+++ src/server.ts
@@ -52,8 +52,25 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello Pooja!')
 });
 
-app.post("/", (req:Request, res:Response)=>{
-    console.log(req.body);
+app.post("/users", async(req:Request, res:Response)=>{
+    const {name,email}=req.body;
+try {
+    const result = await pool.query(`INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`,[name,email]);
+    // console.log(result.rows[0])
+    // res.send({message:"data inserted"})
+    res.status(201).json({
+        success:true,
+        message:"data inserted successfully",
+        data:result.rows[0]
+    })
+    
+} catch (error:any) {
+    res.status(500).json({
+        success:false,
+        message:error.message
+    })
+}
+    
     res.status(201).json({
         success:true,
         message:"API is working"
